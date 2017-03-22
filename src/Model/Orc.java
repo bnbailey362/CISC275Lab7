1ad18f46f0811212e7ec2eff691f76343db33b1b
bnbailey362 <bnbailey@udel.edu>
2017-03-22 14:11:04 -0400

    Added Board and Orc

---
+++ src/Model/Orc.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Orc {
+	static int dir;
+	static int action;
+	int picNum = 0;
+    static int ydir = 1;
+    static int xdir = 1;
+    static int xloc = 0;
+    static int yloc = 0;
+    final int xIncr = 8;
+    final int yIncr = 2;
+    static boolean justChanged = false;
+    final static int imgWidth = 165;
+    final static int imgHeight = 165;
+}
